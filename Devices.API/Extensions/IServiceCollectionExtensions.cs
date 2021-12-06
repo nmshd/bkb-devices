d926e5f4da8776f22e68de2732e2267d6feadb07
tnotheis <timo.notheisen@js-soft.com>
2021-12-06 14:03:20 +0100

    fix: use NullableUtcDateTimeConverter

--- Devices.API/Extensions/IServiceCollectionExtensions.cs
+++ Devices.API/Extensions/IServiceCollectionExtensions.cs
@@ -60,6 +60,7 @@ namespace Devices.API.Extensions
                 .AddJsonOptions(options =>
                 {
                     options.JsonSerializerOptions.Converters.Add(new UtcDateTimeConverter());
+                    options.JsonSerializerOptions.Converters.Add(new NullableUtcDateTimeConverter());
                     options.JsonSerializerOptions.Converters.Add(new UrlSafeBase64ToByteArrayJsonConverter());
                     options.JsonSerializerOptions.Converters.Add(new DeviceIdJsonConverter());
                     options.JsonSerializerOptions.Converters.Add(new IdentityAddressJsonConverter());
