e7cddfddf3d43895f311544f0be240d770fe45d6
tnotheis <timo.notheisen@js-soft.com>
2021-12-06 07:36:16 +0100

    ci: improve publishAdminCli script

--- .ci/publishAdminCli.sh
+++ .ci/publishAdminCli.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-set -x
 set -e
+set -u
+set -x
 
-. "./.ci/shared.sh"
-
-requires APIKEY_NUGET_PUBLISHER
-
-export DOTNET_CLI_HOME=/tmp/DOTNET_CLI_HOME
- 
 rm -rf ./Devices.AdminCli/bin
 rm -rf ./Devices.AdminCli/obj
  
 dotnet restore ./Devices.AdminCli/Devices.AdminCli.csproj
-dotnet build --no-restore ./Devices.AdminCli/Devices.AdminCli.csproj
+dotnet build --configuration Release --no-restore ./Devices.AdminCli/Devices.AdminCli.csproj
 dotnet pack --configuration Release --no-restore --no-build ./Devices.AdminCli/Devices.AdminCli.csproj
 dotnet nuget push ./Devices.AdminCli/bin/Release/*.nupkg -k $APIKEY_NUGET_PUBLISHER -s https://api.nuget.org/v3/index.json --skip-duplicate
