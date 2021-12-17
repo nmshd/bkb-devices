c50c590a577ecfdc68fe461aea700bae8f9bb63e
tnotheis <timo.notheisen@js-soft.com>
2021-12-17 15:29:40 +0100

    refactor: extract methods in NotificationContent

--- Devices.Infrastructure/PushNotifications/NotificationContent.cs
+++ Devices.Infrastructure/PushNotifications/NotificationContent.cs
@@ -15,34 +15,40 @@ public class NotificationContent
     {
         var notificationTypeName = pushNotification.GetType().Name;
 
-        if (!notificationTypeName.Contains(PUSH_NOTIFICATION_POSTFIX))
-        {
-            EventName = "dynamic";
-
-            if (pushNotification is JsonElement jsonElement)
-                Payload = JsonConvert.DeserializeObject<object>(jsonElement.GetRawText());
-            else
-                Payload = pushNotification;
-        }
+        if (notificationTypeName.Contains(PUSH_NOTIFICATION_POSTFIX))
+            HandleStaticContent(pushNotification, notificationTypeName);
         else
-        {
-            EventName = notificationTypeName.Replace(PUSH_NOTIFICATION_POSTFIX, "");
-            Payload = pushNotification;
-        }
+            HandleDynamicContent(pushNotification);
 
         SentAt = SystemTime.UtcNow;
         AccountReference = recipient;
+    }
+
+    private void HandleStaticContent(object pushNotification, string notificationTypeName)
+    {
+        EventName = notificationTypeName.Replace(PUSH_NOTIFICATION_POSTFIX, "");
+        Payload = pushNotification;
+    }
+
+    private void HandleDynamicContent(object pushNotification)
+    {
+        EventName = "dynamic";
+
+        if (pushNotification is JsonElement jsonElement)
+            Payload = JsonConvert.DeserializeObject<object>(jsonElement.GetRawText());
+        else
+            Payload = pushNotification;
     }
 
     [JsonPropertyName("accRef")]
     public string AccountReference { get; }
 
     [JsonPropertyName("eventName")]
-    public string EventName { get; }
+    public string EventName { get; set; }
 
     [JsonPropertyName("sentAt")]
     public DateTime SentAt { get; }
 
     [JsonPropertyName("payload")]
-    public object Payload { get; }
+    public object Payload { get; set; }
 }
