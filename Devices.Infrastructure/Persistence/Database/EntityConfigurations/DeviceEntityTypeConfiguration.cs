3a2275a004a5cec684678cc476b4cef294171edd
tnotheis <timo.notheisen@js-soft.com>
2022-01-26 07:55:31 +0100

    fix: use conventions to specify types of all ids

--- Devices.Infrastructure/Persistence/Database/EntityConfigurations/DeviceEntityTypeConfiguration.cs
+++ Devices.Infrastructure/Persistence/Database/EntityConfigurations/DeviceEntityTypeConfiguration.cs
@@ -9,10 +9,6 @@ public class DeviceEntityTypeConfiguration : IEntityTypeConfiguration<Device>
 {
     public void Configure(EntityTypeBuilder<Device> builder)
     {
-        builder.ToTable("Devices");
-
-        builder.Property(x => x.Id).HasColumnType($"char({DeviceId.MAX_LENGTH})");
-        builder.Property(x => x.DeletedByDevice).HasColumnType($"char({DeviceId.MAX_LENGTH})");
-        builder.Property(x => x.IdentityAddress).HasColumnType($"char({IdentityAddress.MAX_LENGTH})");
+        // builder.ToTable("Devices");
     }
 }
