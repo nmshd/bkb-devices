0167f544fb6e3d2356b381d61431044db864a17e
tnotheis <timo.notheisen@js-soft.com>
2022-02-01 12:04:09 +0100

    fix: use ClientStore to read client from db

--- Devices.API/Controllers/IdentitiesController.cs
+++ Devices.API/Controllers/IdentitiesController.cs
@@ -8,8 +8,10 @@ using Enmeshed.BuildingBlocks.API.Mvc.ControllerAttributes;
 using Enmeshed.BuildingBlocks.Application.Abstractions.Exceptions;
 using IdentityServer4;
 using IdentityServer4.EntityFramework.DbContexts;
-using IdentityServer4.EntityFramework.Entities;
+using IdentityServer4.EntityFramework.Mappers;
 using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Stores;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -22,10 +24,12 @@ namespace Devices.API.Controllers;
 public class IdentitiesController : ApiControllerBase
 {
     private readonly ConfigurationDbContext _identityServerConfigurationDbContext;
+    private readonly IClientStore _clientStore;
 
-    public IdentitiesController(IMediator mediator, ConfigurationDbContext identityServerConfigurationDbContext) : base(mediator)
+    public IdentitiesController(IMediator mediator, ConfigurationDbContext identityServerConfigurationDbContext, IClientStore clientStore) : base(mediator)
     {
         _identityServerConfigurationDbContext = identityServerConfigurationDbContext;
+        _clientStore = clientStore;
     }
 
     [HttpPost]
@@ -34,11 +38,11 @@ public class IdentitiesController : ApiControllerBase
     [AllowAnonymous]
     public async Task<IActionResult> CreateIdentity(CreateIdentityRequest request)
     {
-        var client = await _identityServerConfigurationDbContext.Clients.Include(c => c.ClientSecrets).FirstOrDefaultAsync(c => c.ClientId == request.ClientId);
+        var client = (await _clientStore.FindClientByIdAsync(request.ClientId));
 
         if (client == null || !IsClientSecretCorrect(request, client))
             throw new OperationFailedException(GenericApplicationErrors.Unauthorized());
-
+        
         var command = new CreateIdentityCommand
         {
             ClientId = request.ClientId,
@@ -50,7 +54,35 @@ public class IdentitiesController : ApiControllerBase
                 Signature = request.SignedChallenge.Signature
             }
         };
+        
+        var response = await _mediator.Send(command);
 
+        return Created("", response);
+    }
+
+    [HttpPost("IdentitiesOld")]
+    [ProducesResponseType(typeof(HttpResponseEnvelopeResult<CreateIdentityResponse>), StatusCodes.Status201Created)]
+    [ProducesError(StatusCodes.Status400BadRequest)]
+    [AllowAnonymous]
+    public async Task<IActionResult> CreateIdentity2(CreateIdentityRequest request)
+    {
+        var client = await _identityServerConfigurationDbContext.Clients.Include(c => c.ClientSecrets).FirstOrDefaultAsync(c => c.ClientId == request.ClientId);
+
+        if (client == null || !IsClientSecretCorrect(request, client.ToModel()))
+            throw new OperationFailedException(GenericApplicationErrors.Unauthorized());
+        
+        var command = new CreateIdentityCommand
+        {   
+            ClientId = request.ClientId,
+            DevicePassword = request.DevicePassword,
+            IdentityPublicKey = request.IdentityPublicKey,
+            SignedChallenge = new SignedChallengeDTO
+            {
+                Challenge = request.SignedChallenge.Challenge,
+                Signature = request.SignedChallenge.Signature
+            }
+        };
+        
         var response = await _mediator.Send(command);
 
         return Created("", response);
